cc632600c75b136d89fde6dad774f31bb417b3f3
Aymeric Aillet <aymeric.aillet@iot.bzh>
2020-10-12 08:11:25 +0000

    Clear redtest and test sripts output
    Task 1650
    
    Signed-off-by: Aymeric Aillet <aymeric.aillet@iot.bzh>

--- test/afb-test/fixtures/run-test.sh
+++ test/afb-test/fixtures/run-test.sh
@@ -11,10 +11,10 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function stop_gpsd {
-        sudo systemctl stop gpsd.service
-        sudo systemctl stop gpsd.socket
-        sudo pkill -9 gpsfake
-        sudo pkill -9 gpsd
+        sudo systemctl stop gpsd.service >/dev/null
+        sudo systemctl stop gpsd.socket >/dev/null
+        sudo pkill -9 gpsfake >/dev/null
+        sudo pkill -9 gpsd >/dev/null
 }
 
 echo "--- Stopping remaining gpsd & gpsfake instances ---"
