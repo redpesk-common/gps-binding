91307978c227984245e99d2379d070efc82039d2
José Bollo <jose.bollo@iot.bzh>
2026-03-09 15:26:40 +0100

    Version 2.0.1

--- binding/json_info.c
+++ binding/json_info.c
@@ -4,7 +4,7 @@ const char *info_verbS =
     "\"metadata\": {"
         "\"uid\": \"gps\","
         "\"info\": \"GPS Binding\","
-        "\"version\": \"1.0\""
+        "\"version\": \"2.0.1\""
     "},"
     "\"groups\": ["
         "{"
